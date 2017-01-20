b60aa9df0aaf68d93780e86d416198c91688d931
Amir Mahdi Hosseini Monazzah <amir@ubuntu.(none)>
2017-01-19 20:17:16 -0800

    first commit

---
+++ Majid/scale/src/approximations.h
@@ -0,0 +1,6 @@
+//Approximation Options
+#include "/home/amir/AMHM_Approximation_Benchmarks/Majid/scale/src/m5op.h" // MJ
+
+#define SRC  // input image
+#define DEST // output image
+int reliability_level;
